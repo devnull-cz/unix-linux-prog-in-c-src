fa30aefed5c9cd9cc8dc9cbf629ae175ddcf4ad4
Jan Pechanec <jp@devnull.cz>
2009-10-14 22:35:33 +0200

    Enhanced.

--- main/print-argv.c
+++ main/print-argv.c
@@ -11,8 +11,14 @@ main(int argc, char **argv)
 {
 	int i = 0;
 
+	/* You can do it both ways. */
+#if 1
 	while (argv[i] != NULL)
 		printf("%s\n", argv[i++]);
+#else
+	for (i = 0; i < argc; ++i)
+		printf("%s\n", argv[i]);
+#endif
 
 	return (0);
 }
