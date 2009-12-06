e6b5a76c18b3e0fb39f2609add5b7571705479b4
Jan Pechanec <jp@devnull.cz>
2009-12-06 18:35:25 +0100

    Fixed cstyle, SIGHUP changed to SIGINT.

--- signals/signal-vs-sigaction.c
+++ signals/signal-vs-sigaction.c
@@ -4,8 +4,8 @@
  *   1. run the program
  *      ./sig2
  *   2. send the 2 signals in quick succession (in the 10 seconds window)
- *      kill -HUP `pgrep sig2`
- *      kill -HUP `pgrep sig2`
+ *      kill -INT `pgrep sig2`
+ *      kill -INT `pgrep sig2`
  *   3. observe the behavior
  *
  *  Vladimir Kotal, 2009
@@ -16,7 +16,9 @@
 #include <unistd.h>
 #include <sys/types.h>
 
-void sig_handler(int s) {
+void
+sig_handler(int s)
+{
 	printf("Got signal! Sleeping.\n");
 
 	sleep(10);
@@ -24,23 +26,30 @@ void sig_handler(int s) {
         printf("returning from signal handler\n");
 }
 
-int main(void) {
+int
+main(void)
+{
 	struct sigaction s_action;
 
+	/*
+	 * You may experiment with other signals but note that some shell might
+	 * automatically block some signals; bash(1) sometimes block SIGHUP in
+	 * the new process, for example.
+	 */
 	printf("Setting signal handler: ");
 #ifdef POSIX_SIGNALS
 	printf("sigaction\n");
 	(void) sigemptyset(&s_action.sa_mask);
 	s_action.sa_handler = sig_handler;
 	s_action.sa_flags = 0;
-	(void) sigaction(SIGHUP, &s_action, (struct sigaction *) NULL);
+	(void) sigaction(SIGINT, &s_action, (struct sigaction *)NULL);
 #else
 	printf("signal\n");
-	signal(SIGHUP, sig_handler);
+	signal(SIGINT, sig_handler);
 #endif
 
 	printf("Waiting for signal\n");
-	while(1)
+	while (1)
 		pause();
 
 	return (0);
