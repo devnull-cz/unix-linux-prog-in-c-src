bb0bce9743eec8e6d5edcd98ab1a9e57c0da194a
Jan Pechanec <jp@devnull.cz>
2009-12-13 12:34:02 +0100

    New examples.

---
+++ select/connect-only.c
@@ -0,0 +1,50 @@
+/*
+ * Connect only. To show what happens if the remote side starts writing but we
+ * do not read. Use with select/write-select.c.
+ *
+ * (c) jp@devnull.cz
+ */
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <err.h>
+#include <strings.h>
+
+#define	BUF_LEN	100
+
+int
+main(int argc, char **argv)
+{
+	int fd;
+	struct sockaddr_in in;
+
+	if (argc != 3)
+		errx(1, "usage: %s <ip-address> <port>", argv[0]);
+
+	bzero(&in, sizeof (in));
+	in.sin_family = AF_INET;
+	in.sin_port = htons(atoi(argv[2]));
+
+	/* Convert a dotted format to 4 bytes suitable for use in sockaddr_in. */
+	if (inet_pton(AF_INET, argv[1], &(in.sin_addr)) != 1)
+		errx(1, "inet_pton failed for '%s'", argv[1]);
+
+	if ((fd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+		err(1, "socket");
+	
+	if (connect(fd, (struct sockaddr *) &in, sizeof(in)) == -1)
+		err(1, "connect");
+
+	/* Wait for ever. */
+	while (1)
+		pause();
+
+	/* Not reached. */
+	return (0);
+}
---
+++ select/write-select.c
@@ -0,0 +1,72 @@
+/*
+ * Example on 
+ * (c) jp@devnull.cz
+*/
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <err.h>
+#include <errno.h>
+
+#define	BUF_LEN	1024
+
+int
+main(int argc, char **argv)
+{
+	fd_set wrfds;
+	char buf[BUF_LEN];
+	struct sockaddr_in sa;
+	int s, newsock, n, total = 0, optval = 1;
+
+	if (argc != 2)
+		errx(1, "usage: %s <port>", argv[0]);
+
+	if ((s = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+		err(1, "socket");
+
+	sa.sin_family = AF_INET;
+	sa.sin_port = htons((short) atoi(argv[1]));
+	sa.sin_addr.s_addr = htonl(INADDR_ANY);
+
+	memset(buf, '.', BUF_LEN);
+	
+	setsockopt(s, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval));
+	if (bind(s, (struct sockaddr *) &sa, sizeof(sa)) == -1)
+		err(1, "bind");
+
+	if (listen(s, SOMAXCONN) == -1)
+		err(1, "listen");
+
+	if ((newsock = accept(s, NULL, 0)) == -1)
+		err(1, "accept");
+
+	/*
+	 * Just write data to the remote side. If the remote side does not read
+	 * the data we should be finally put to sleep in select().
+	 */
+	for ( ; ; ) {
+		/* Must do this each time before calling select(). */
+		FD_ZERO(&wrfds);
+		FD_SET(newsock, &wrfds);
+		if (select(newsock + 1, NULL, &wrfds, NULL, NULL) == -1)
+			err(1, "select");
+
+		if (FD_ISSET(newsock, &wrfds)) {
+			if ((n = write(newsock, buf, BUF_LEN)) == -1)
+				err(1, "write (%d)", errno);
+			total = total + n;
+			fprintf(stderr, "[ %d bytes written (total %d) ]", n, total);
+		}
+	}
+
+	close(newsock);
+	close(s);
+	return (0);
+}
