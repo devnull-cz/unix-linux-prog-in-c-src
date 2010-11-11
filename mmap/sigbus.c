442262943deaf5b9da62c932e7b48c9b7b39b8a3
Jan Pechanec <jp@devnull.cz>
2010-11-11 01:01:54 +0100

    Drobny zmeny v sigbus.c.

--- mmap/sigbus.c
+++ mmap/sigbus.c
@@ -1,3 +1,8 @@
+/*
+ * Map a file and show what happens if one accesses data after the mapped chunk.
+ * Mapping is done in page granularity so reaching after the mapped chunk is OK
+ * as long we access the same page. x86 has 4KB pages.
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
@@ -17,6 +22,7 @@ int main(void)
 
 	addr = mmap(0, 100, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
 
+	/* On x86, accesing 4095 is OK, 4096 generates SIGSEGV or SIGBUS. */
 	//addr[4095] = 0;
 	addr[4096] = 0;
 
