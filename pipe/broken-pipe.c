af6848a710766a7fdac21b6578e7815a3c5a3025
Vladimir Kotal <vlada@devnull.cz>
2011-11-24 12:39:43 +0100

    demonstrate that err() will not be called because of SIGPIPE

--- pipe/broken-pipe.c
+++ pipe/broken-pipe.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <err.h>
 
-int main(void)
+int
+main(void)
 {
 	int pd[2];
 	char c = (char) 'a';
 
 	pipe(pd);
 	close(pd[0]);
-	write(pd[1], &c, 1);
+	if (write(pd[1], &c, 1) == -1)
+		err(1, "write");
 
 	return (0);
 }
