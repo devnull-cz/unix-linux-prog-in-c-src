12a000ec156a8d4de620567b915a85a7c4221df5
Vladimir Kotal <vlada@devnull.cz>
2011-11-24 12:44:15 +0100

    C style

--- pipe/main.c
+++ pipe/main.c
@@ -2,33 +2,34 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-int main(void)
+int
+main(void)
 {
-  int pid;
-  int pd[2];
+	int pid;
+	int pd[2];
 
-  pipe(pd);
+	pipe(pd);
 
-  switch (pid = fork()) {
-    case -1:
-      fprintf(stderr, "error\n");
-      exit(1);
-    case 0:
-      close(1);
-      dup(pd[1]);
-      // not before dup() !
-      close(pd[0]);
-      close(pd[1]);
-      execl("/bin/ls", "/bin/ls", "/", (char *) NULL);
-      break;
-    default:
-      close(0);
-      dup(pd[0]);
-      close(pd[0]);
-      close(pd[1]);
-      execl("/usr/bin/less", "/usr/bin/less", (char *) NULL);
-      break;
-  }
+	switch (pid = fork()) {
+	case -1:
+		fprintf(stderr, "error\n");
+		exit(1);
+	case 0:
+		close(1);
+		dup(pd[1]);
+		// not before dup() !
+		close(pd[0]);
+		close(pd[1]);
+		execl("/bin/ls", "/bin/ls", "/", (char *) NULL);
+		break;
+	default:
+		close(0);
+		dup(pd[0]);
+		close(pd[0]);
+		close(pd[1]);
+		execl("/usr/bin/less", "/usr/bin/less", (char *) NULL);
+		break;
+	}
 
-  return 0;
+	return (0);
 }
