65cb8d6e560e4c6ad42bb932f7befdaa3c01fe47
Vladimir Kotal <vlada@devnull.cz>
2011-12-15 12:00:50 +0100

    make the dlopen() example do something useful - demonstrate the difference between RTLD_NOW and RTLD_LAZY. PIC library is needed for that.

--- dyn-lib/dlopen.c
+++ dyn-lib/dlopen.c
@@ -1,32 +1,43 @@
 /*
  * A simple program to show dlopen/dlsym functions. You need libmy.c which
- * should be in the same directory as this file. You build it like this (don't
- * forget -L/-R options):
+ * should be in the same directory as this file. Build it like this:
  *
  * Solaris:
- *  $ cc -o libmy.so -G libmy.c
+ *  $ cc -o libmy.so -KPIC -G libmy.c
  *  $ cc -L. -R. dlopen.c
  *
  * Linux:
- *  $ gcc -shared -o libmy.so libmy.c
+ *  $ gcc -shared -fPIC -o libmy.so libmy.c
  *  $ gcc -L. -Xlinker -R . -ldl dlopen.c
  *
- * (c) jp@devnull.cz
+ * (c) jp@devnull.cz, vlada@devnull.cz
  */
 
-#include <dlfcn.h>
-#include <errno.h>
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
+#include <errno.h>
 #include <err.h>
+#include <dlfcn.h>
 
-int main(void)
+int main(int argc, char *argv[])
 {
 	typedef void (*fn)(void);
 	void *handle;
 	fn f;
+	int flag;
 
-	if ((handle = dlopen("./libmy.so", RTLD_LAZY)) == NULL)
+	if (argc != 2)
+		errx(1, "usage: %s <now|lazy>", argv[0]);
+
+	if (!strcmp(argv[1], "now"))
+		flag = RTLD_NOW;
+	else if (!strcmp(argv[1], "lazy"))
+		flag = RTLD_LAZY;
+	else
+		errx(1, "invalid flag value");
+
+	if ((handle = dlopen("./libmy.so", flag)) == NULL)
 		errx(1, "dlopen: %s", dlerror());
 
 	if ((f = (fn) dlsym(handle, "myprint")) == NULL)
--- dyn-lib/libmy.c
+++ dyn-lib/libmy.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
+extern void foo(void);
+
 void myprint(void)
 {
 	printf("hello\n");
+	foo();
 }
