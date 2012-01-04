34f1be7abfa616c034a1802b453c22d158d7c10e
Vladimir Kotal <vlada@devnull.cz>
2012-01-04 22:03:20 +0100

    add comment about gethostbyadrr() being obsolete

--- tcp/gethostbyaddr.c
+++ tcp/gethostbyaddr.c
@@ -1,11 +1,19 @@
+/*
+ * Example on how NOT to resolve IP address to hostnames.
+ *
+ * Instead of gethostbyaddr() use getaddrinfo() because it is standard
+ * function.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
 #include <netdb.h>
-#include <stdlib.h>
-#include <stdio.h>
 
 int
 main(int argc, char **argv)
@@ -15,7 +23,7 @@ main(int argc, char **argv)
 	struct hostent *hp;
 
 	if (argc != 2)
-		exit(1);
+		errx(1, "usage: %s <ip_address>", argv[0]);
 
 	inet_aton(argv[1], &ip);
 	hp = gethostbyaddr((char *) &ip, sizeof(ip), AF_INET);
