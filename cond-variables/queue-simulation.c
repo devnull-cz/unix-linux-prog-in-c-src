b34c403d75c1cafbb4bf13a72e66b39a01f29b2e
Vladimir Kotal <vlada@devnull.cz>
2012-01-12 13:00:53 +0100

    cstyle

--- cond-variables/queue-simulation.c
+++ cond-variables/queue-simulation.c
@@ -100,7 +100,7 @@ main(int argc, char **argv)
 
 	pthread_create(&t, NULL, producer, NULL);
 	pthread_create(&t, NULL, consumer, NULL);
-	
+
 	/*
 	 * Main thread. Periodically print the "contents" of the queue.
 	 */
