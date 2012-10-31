ca2590e135f53ceb5024678e78b3fec8cce7108e
Jan Pechanec <jp@devnull.cz>
2012-10-31 09:03:22 +0100

    Minor change for setuid(2) example.

--- setuid/screate-file.c
+++ setuid/screate-file.c
@@ -8,30 +8,37 @@
 
 #include <fcntl.h>
 #include <unistd.h>
+#include <assert.h>
+#include <sys/stat.h>
 
 int
 main(void)
 {
+	int ret;
+
 	/*
 	 * let's pretend the directory and the file do not exist and that
 	 * nothing can go wrong.
 	 */
 
 	/* create the directory with you as the owner */
-	mkdir("/tmp/abc", 0777);
+	ret = mkdir("/tmp/abc", 0777);
+	assert(ret == 0);
 
 	/*
 	 * make sure the user will have enough privileges to create a file
 	 * in here (remember - after we switch EUID to his/her own one we
 	 * can't normally create files in somebody else's directories).
 	 */
-	chmod("/tmp/abc", 0777);
+	ret = chmod("/tmp/abc", 0777);
+	assert(ret == 0);
 
 	/*
 	 * create the directory with the user who started the program as the
 	 * owner.
 	 */
-	setuid(getuid());
+	ret = setuid(getuid());
+	assert(ret == 0);
 	close(open("/tmp/abc/123", O_RDONLY | O_CREAT, 0666));
 
 	return (0);
