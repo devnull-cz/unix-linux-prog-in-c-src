74100e5e6831a6bf36b45305bcebfa68a2271c62
Vladimir Kotal <vlada@devnull.cz>
2012-12-05 00:08:21 +0100

    make the example a bit more useful

--- resolving/getbyname.c
+++ resolving/getbyname.c
@@ -18,15 +18,29 @@ main(int argc, char **argv)
 	char buf[100];
 	int fd, newsock, n;
 
-	if (argc != 2)
-		errx(1, "usage: %s <port>", argv[0]);
+	if (argc != 3)
+		errx(1, "usage: %s <service> <protocol_name>", argv[0]);
 
-	sp = getservbyname(argv[1], "tcp");
-	pp = getprotobyname("tcp");
+	/* get the protocol entry first as we will need it for service lookup */
+	pp = getprotobyname(argv[2]);
+	if (pp == NULL)
+		errx(1, "'%s' not valid protocol name", argv[2]);
+
+	/* now lookup service for the protocol */
+	sp = getservbyname(argv[1], argv[2]);
+	if (sp == NULL) {
+		printf("'%s' not a known service name, trying port number\n",
+		    argv[1]);
+		sp = getservbyport(atoi(argv[1]), argv[2]);
+		if (sp == NULL)
+			errx(1, "not valid service port number either");
+	}
+
+	printf("official service name: %s\n", sp->s_name);
 	fd = socket(AF_INET, SOCK_STREAM, pp->p_proto);
 
 	sa.sin_family = AF_INET;
-	/* do NOT use htons() here ! */
+	/* do NOT use htons() here, it's already in network byte order ! */
 	sa.sin_port = sp->s_port;
 	sa.sin_addr.s_addr = INADDR_ANY;
 
