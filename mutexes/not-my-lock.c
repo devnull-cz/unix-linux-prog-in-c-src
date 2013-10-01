6c90e013679f4619cd279f8a2dd1b02244a06758
Jan Pechanec <jp@devnull.cz>
2013-09-30 23:01:38 -0700

    Fix cstyle, lint, and some minor logical nits.

--- mutexes/not-my-lock.c
+++ mutexes/not-my-lock.c
@@ -14,6 +14,7 @@
 #include <stdlib.h>
 #include <err.h>
 #include <string.h>
+#include <assert.h>
 
 /* Needed for Linux to include the necessary mutex types */
 #define	__USE_UNIX98
@@ -26,14 +27,13 @@ pthread_mutex_t mutex, mutex2, mutex3;
 void
 usage()
 {
-	printf("Program messes up with mutexes.\n");
-	printf("Usage: not-my-lock [-h|-e|-n|-r]\n");
-	printf("-e\tset mutex type to ERRORCHECK\n");
-	printf("-n\tset mutex type to NORMAL\n");
-	printf("-r\tset mutex type to RECURSIVE\n");
-	printf("-h\tthis usage\n");
-	printf("With no option: mutex type is set to DEFAULT.\n");
-
+	(void) printf("Program messes up with mutexes.\n");
+	(void) printf("Usage: not-my-lock [-h|-e|-n|-r]\n");
+	(void) printf("-e\tset mutex type to ERRORCHECK\n");
+	(void) printf("-n\tset mutex type to NORMAL\n");
+	(void) printf("-r\tset mutex type to RECURSIVE\n");
+	(void) printf("-h\tthis usage\n");
+	(void) printf("With no option: mutex type is set to DEFAULT.\n");
 	exit(1);
 }
 
@@ -43,7 +43,7 @@ thread(void *x)
 	int e;
 
 	sleep(1); /* Give the main thread some time to lock the mutex. */
-	fprintf(stderr, "Attempting to unlock a mutex owned by somebody "
+	(void) fprintf(stderr, "Attempting to unlock a mutex owned by somebody "
 	    "else...\n");
 
 	/*
@@ -51,9 +51,9 @@ thread(void *x)
 	 * mutex type is NORMAL while it's ERRORCHECK on FreeBSD.
 	 */
 	if ((e = pthread_mutex_unlock(&mutex)) != 0)
-		fprintf(stderr, "%s\n\n", strerror(e));
+		(void) fprintf(stderr, "%s\n\n", strerror(e));
 	else
-		fprintf(stderr, "No error detected.\n\n");
+		(void) fprintf(stderr, "No error detected.\n\n");
 
 	return (NULL);
 }
@@ -63,39 +63,43 @@ main(int argc, char *argv[])
 {
 	pthread_t t;
 	pthread_mutexattr_t attr;
-	int e, opt;
+	int e, opt, ret;
 
-	pthread_mutexattr_init(&attr);
+	(void) pthread_mutexattr_init(&attr);
 
 	while ((opt = getopt(argc, argv, "renh")) != -1) {
 		switch (opt) {
-			case 'n':
-				if (pthread_mutexattr_settype(&attr,
-				    PTHREAD_MUTEX_NORMAL) != 0)
-					err(1, "mutexattr_settype");
-				break;
-			case 'r':
-				if (pthread_mutexattr_settype(&attr,
-				    PTHREAD_MUTEX_RECURSIVE) != 0)
-					err(1, "mutexattr_settype");
-				break;
-			case 'e':
-				if (pthread_mutexattr_settype(&attr,
-				    PTHREAD_MUTEX_ERRORCHECK) != 0)
-					err(1, "mutexattr_settype");
-				break;
-			case 'h':
-				usage();
-				break;
-			default:
-				if (pthread_mutexattr_settype(&attr,
-				    PTHREAD_MUTEX_DEFAULT) != 0)
-					err(1, "mutexattr_settype");
-				warnx("Mutex set to default type");
-				warnx("Run with -h to see other possible " \
-				    "types");
-				break;
+		case 'n':
+			if (pthread_mutexattr_settype(&attr,
+			    PTHREAD_MUTEX_NORMAL) != 0) {
+				err(1, "mutexattr_settype");
+			}
+			break;
+		case 'r':
+			if (pthread_mutexattr_settype(&attr,
+			    PTHREAD_MUTEX_RECURSIVE) != 0) {
+				err(1, "mutexattr_settype");
+			}
+			break;
+		case 'e':
+			if (pthread_mutexattr_settype(&attr,
+			    PTHREAD_MUTEX_ERRORCHECK) != 0) {
+				err(1, "mutexattr_settype");
+			}
+			break;
+		case 'h':
+		default:
+			usage();
+			break;
 		}
+	}
+
+	if (argc == 1) {
+		ret = pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_DEFAULT);
+		if (ret != 0)
+			err(1, "mutexattr_settype");
+		warnx("Mutex set to default type");
+		warnx("Run with -h to see other possible types\n");
 	}
 
 	if (pthread_mutex_init(&mutex, &attr) == -1)
@@ -105,28 +109,29 @@ main(int argc, char *argv[])
 	if (pthread_mutex_init(&mutex3, &attr) == -1)
 		err(1, "mutex_init for mutex3");
 
-	pthread_create(&t, NULL, thread, (void *) 1);
-	fprintf(stderr, "Locking a mutex (and sleep 1 second).\n");
-	pthread_mutex_lock(&mutex);
+	ret = pthread_create(&t, NULL, thread, (void *)1);
+	assert(ret == 0);
+	(void) fprintf(stderr, "Locking a mutex (and sleep 1 second).\n");
+	(void) pthread_mutex_lock(&mutex);
 	pthread_join(t, NULL);
 
-	fprintf(stderr, "Unlocking an unlocked mutex2.\n");
+	(void) fprintf(stderr, "Unlocking an unlocked mutex2.\n");
 	if ((e = pthread_mutex_unlock(&mutex2)) != 0)
-		fprintf(stderr, "%s\n\n", strerror(e));
+		(void) fprintf(stderr, "%s\n\n", strerror(e));
 	else
-		fprintf(stderr, "No error detected.\n\n");
+		(void) fprintf(stderr, "No error detected.\n\n");
 
-	fprintf(stderr, "Locking mutex3.\n");
-	pthread_mutex_lock(&mutex3);
-	fprintf(stderr, "And locking it again (will we deadlock?).\n");
+	(void) fprintf(stderr, "Locking mutex3.\n");
+	(void) pthread_mutex_lock(&mutex3);
+	(void) fprintf(stderr, "And locking it again (will we deadlock?).\n");
 	if ((e = pthread_mutex_lock(&mutex3)) != 0)
-		fprintf(stderr, "%s\n\n", strerror(e));
+		(void) fprintf(stderr, "%s\n\n", strerror(e));
 	else
-		fprintf(stderr, "No error detected.\n\n");
+		(void) fprintf(stderr, "No error detected.\n\n");
 
-	pthread_mutex_destroy(&mutex);
-	pthread_mutex_destroy(&mutex2);
-	pthread_mutex_destroy(&mutex3);
+	(void) pthread_mutex_destroy(&mutex);
+	(void) pthread_mutex_destroy(&mutex2);
+	(void) pthread_mutex_destroy(&mutex3);
 
 	return (0);
 }
