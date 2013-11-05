5d852e0696297ff453affd54c146dc127dc53bf9
Vladimir Kotal <vlada@devnull.cz>
2013-11-05 13:46:12 +0100

    add explanatory comment

--- pipe/main.c
+++ pipe/main.c
@@ -1,3 +1,16 @@
+/*
+ * Basic example on how to construct pipe. Notice especially the following:
+ *   - write end of the pipe is closed in consumer
+ *     - this is so that all references to it are closed once the producer is
+ *       done and so that the consumer can get proper EOF
+ *   - read end of the pipe is closed in producer
+ *     - this is so that if the consumer ends up prematurely the producer
+ *       will get SIGPIPE (otherwise it would fill the pipe buffer and block
+ *       indefinitely)
+ *   - pipe is constructed from producer to consumer (data flow - wise)
+ *
+ */
+
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -14,7 +27,7 @@ main(void)
 	case -1:
 		fprintf(stderr, "error\n");
 		exit(1);
-	case 0:
+	case 0: /* producer */
 		close(1);
 		dup(pd[1]);
 		// not before dup() !
@@ -22,7 +35,7 @@ main(void)
 		close(pd[1]);
 		execl("/bin/ls", "/bin/ls", "/", (char *) NULL);
 		break;
-	default:
+	default: /* consumer */
 		close(0);
 		dup(pd[0]);
 		close(pd[0]);
