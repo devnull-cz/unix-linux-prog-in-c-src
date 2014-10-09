4f1503661a7b1c1b0104694f88381363e0a2cbbe
Vladimir Kotal <vlada@devnull.cz>
2014-10-09 15:55:04 +0200

    make it work on Solaris 10

--- basic-utils/preload.c
+++ basic-utils/preload.c
@@ -10,8 +10,13 @@
 #include <unistd.h>
 #include <stdio.h>
 
+/* On Solaris cat(1) calls fclose() which in turn calls _close(). */
 int
+#ifdef __sun
+_close(int d)
+#else
 close(int d)
+#endif
 {
 	(void) printf("fd %d will not be closed!\n", d);
 	return (0);
