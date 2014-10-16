27f99fa6207443a0b19cd2d5079a75322fa94d61
Vladimir Kotal <vlada@devnull.cz>
2014-10-16 12:56:16 +0200

    demonstrate the use of strdup() and shell argument separation

--- getopt/getopt.c
+++ getopt/getopt.c
@@ -2,10 +2,14 @@
  * An example on getopt(). See getopts.sh which is a shell counterpart to this
  * C code.
  *
+ * Run e.g. like this:
+ *   ./a.out foo -c aaa bar "har haha"
+ *
  * (c) jp@devnull.cz, vlada@devnull.cz
  */
 
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <libgen.h>
@@ -25,6 +29,7 @@ main(int argc, char **argv)
 {
 	int opt;
 	char *argv0 = basename(argv[0]);
+	char *code = NULL;
 
 	/* Comment out to print getopt() error messages. */
 	opterr = 0;
@@ -40,7 +45,14 @@ main(int argc, char **argv)
 	while ((opt = getopt(argc, argv, "c:")) != -1) {
 		switch (opt) {
 		case 'c':
-			printf("option -c set to '%s'\n", optarg);
+			/*
+			 * optarg should be copied since it might be
+			 * overwritten by another option or freed by getopt()
+			 */
+			if ((code = strdup(optarg)) == NULL) {
+				printf("cannot alloc memory for -c optarg\n");
+				exit(1);
+			}
 			break;
 		case '?':
 			printf("unknown option: '%c'\n", optopt);
@@ -51,6 +63,9 @@ main(int argc, char **argv)
 
 	printf("...done reading option arguments\n");
 
+	if (code != NULL)
+		printf("option -c set to '%s'\n", code);
+
 	/* optind is the 1st non-option argument */
 	argv += optind;
 	argc -= optind;
@@ -58,7 +73,7 @@ main(int argc, char **argv)
 	if (argc >= 1) {
 		printf("filenames:");
 		while (argv[0] != NULL) {
-			printf(" %s", argv[0]);
+			printf(" '%s'", argv[0]);
 			++argv;
 		}
 		printf("\n");
