086db303a3a6d6e4aa6889acf0b3d9379549ddf5
Vladimir Kotal <vlada@devnull.cz>
2015-10-15 14:17:50 +0200

    POSIX comment

--- basic-utils/suvreq.c
+++ basic-utils/suvreq.c
@@ -1,6 +1,7 @@
 /*
  * This define is used to request the implementation make available the symbols
  * and prototypes of Single UNIX Specification V3.
+ * It subsumes the definition of the POSIX macro _POSIX_C_SOURCE=200112L
  */
 #define	_XOPEN_SOURCE	600
 
