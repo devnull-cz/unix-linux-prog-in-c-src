9deca7248c7c58aeae4c0f74f09e29484b0e83a6
Vladimir Kotal <vlada@devnull.cz>
2015-10-15 14:16:17 +0200

    comment about meaning of _XOPEN_SOURCE 600

--- basic-utils/suvreq.c
+++ basic-utils/suvreq.c
@@ -1,3 +1,7 @@
+/*
+ * This define is used to request the implementation make available the symbols
+ * and prototypes of Single UNIX Specification V3.
+ */
 #define	_XOPEN_SOURCE	600
 
 #include <stdio.h>
