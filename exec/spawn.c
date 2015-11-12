50f65d752b94f712aafac8b34964e97adee33e93
Vladimir Kotal <vlada@devnull.cz>
2015-11-12 18:38:36 +0100

    add simple posix_spawn() example

---
+++ exec/spawn.c
@@ -0,0 +1,50 @@
+/*
+ * Simple posix_spawn() demonstration.
+ *
+ * (c) vlada@devnull.cz
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <spawn.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+static void
+spawnit(char *progname)
+{
+	int err, status;
+	pid_t pid;
+	char *argv[3] = { "ps", "-j", NULL };
+
+	if ((err = posix_spawn(&pid, progname, NULL, NULL, argv,
+	    NULL)) != 0) {
+		fprintf(stderr, "posix_spawn for %s: %d\n", progname, err);
+		return;
+	}
+
+	/*
+	 * Error detection is not easy with posix_spawn(). It can happen that
+	 * the child process fails to execute the program after posix_spawn()
+	 * returned success.
+	 *
+	 * NOTE: Checking with WNOHANG is not reliable as this is scheduling
+	 *	 dependent !
+	 */
+	waitpid(pid, &status, 0);
+	if (WEXITSTATUS(status) == 127) {
+		fprintf(stderr, "posix_spawn failed for %s\n", progname);
+		return;
+	}
+
+	printf("pid = %d\n", pid);
+}
+
+int
+main(void)
+{
+	spawnit("/bin/foo");
+	spawnit("/bin/ps");
+
+	return (0);
+}
