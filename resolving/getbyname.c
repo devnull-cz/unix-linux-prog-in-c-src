061e335d12dbb50a9f3a291142da98b55d98cb51
Vladimir Kotal <vlada@devnull.cz>
2015-12-10 13:32:54 +0100

    add missing includes, reorder

--- resolving/getbyname.c
+++ resolving/getbyname.c
@@ -2,12 +2,14 @@
  * Demonstrate the use of getservbyname() and getprotobyname().
  */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <err.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
-#include <stdio.h>
-#include <unistd.h>
 
 int
 main(int argc, char **argv)
