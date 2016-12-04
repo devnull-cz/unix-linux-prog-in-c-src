a6263d1384f55bffae8690a6c82874514970f953
Jan Pechanec <jp@devnull.cz>
2016-12-04 19:55:49 +0100

    Fix the comment.

--- pthreads/setjmp.c
+++ pthreads/setjmp.c
@@ -2,15 +2,16 @@
  * A very simple example on how user thread library could be written. Remember
  * that library threads can be used even on systems that don't support threads
  * in the kernel. Basically, the user thread library would change all blocking
- * calls to nonblocking and if the call indicates that it would block then the
- * process state is saved and one of previously saved states is used for a
- * long jump. Obviously, it's not easy to write such a library but you get the
- * general idea on how to do it.
+ * calls to nonblocking and if a call indicates that it would block the process
+ * state is saved and one of previously saved states is used for a long jump.
+ * Obviously, it's not easy to write such a library but you get the general idea
+ * on how to do it.
  *
  * The program saves the state and prints "0". The first "if" is not satisfied
  * so we longjump() to the saved state. "1" is printed but since "i" is 2 when
  * the check is performed, the program exists. So, as you can see, without any
- * loop or goto statement we print twice using just one fprintf() in the code.
+ * loop or goto statement we print output twice using just one fprintf() in the
+ * code.
  *
  * (c) jp@devnull.cz, vlada@devnull.cz
  */
