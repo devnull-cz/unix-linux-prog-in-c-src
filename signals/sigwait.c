ffebb050db51fe769aadee46550cc3460f5b4b3d
Jan Pechanec <jp@devnull.cz>
2017-11-19 22:19:27 +0100

    Cleaned up.

--- signals/sigwait.c
+++ signals/sigwait.c
@@ -1,11 +1,11 @@
 /*
- * Two processes share a memory and they synchronize with SIGUSR1. When the
- * father reads a character from the terminal, it writes as the 1st character of
- * the shared memory and sends a signal to its child.
+ * Two processes share a piece of memory and synchronize with SIGUSR1.  When the
+ * parent reads a character from the terminal, it puts it to the shared memory
+ * and sends a signal to its child.
  *
- * The child is waiting in sigwait(). When the signals comes it reads the memory
- * and prints the character to stdout. Note that we use sigwait() which doesn't
- * use handlers.
+ * The child is waiting in sigwait().  When the signal is delivered  it reads
+ * the memory and prints the character to stdout.  Note that we use sigwait()
+ * which does not use handlers at all.
  *
  * On Solaris, you must compile with "-D_POSIX_PTHREAD_SEMANTICS" since by
  * default sigwait() uses only the 1st parameter there.
@@ -17,25 +17,26 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <assert.h>
 #include <err.h>
 
 int
 main(void)
 {
 	char c = 0;
-	int fd, pid, sig;
 	char *addr;
 	sigset_t set;
+	int fd, pid, sig;
 
 	if ((fd = open("test.dat", O_CREAT | O_TRUNC | O_RDWR, 0666)) == -1)
 		err(1, "open");
 
-	/* extend the file */
+	/* Extend the file. */
 	if (write(fd, &c, 1) != 1)
 		err(1, "write");
 
 	addr = mmap(0, 1, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-	if ((void *)addr == MAP_FAILED)
+	if (addr == MAP_FAILED)
 		err(1, "mmap");
 
 	sigemptyset(&set);
@@ -43,21 +44,23 @@ main(void)
 	sigprocmask(SIG_SETMASK, &set, NULL);
 
 	switch (pid = fork()) {
-		case -1:
-			perror("fork");
-			exit(1);
-		case 0:
-			printf("%d: this is child\n", getpid());
-			sigwait(&set, &sig);
-			printf("%d: got '%c'\n", getpid(), addr[0]);
-			break;
-		default:
-			printf("%d: this is father\n", getpid());
-			printf("type anything (only 1st char matters)...\n");
-			if (read(0, &c, 1) != 1)
-				err(1, "read");
-			addr[0] = c;
-			kill(pid, SIGUSR1);
+	case -1:
+		err(1, "fork");
+	case 0:
+		printf("%d: this is a child\n", getpid());
+		sigwait(&set, &sig);
+		assert(sig == SIGUSR1);
+		printf("%d: got '%c'\n", getpid(), addr[0]);
+		break;
+	default:
+		printf("%d: this is a parent\n", getpid());
+		printf("%d: type anything (only the 1st char matters)...\n",
+		    getpid());
+		if (read(0, &c, 1) != 1)
+			err(1, "read");
+		addr[0] = c;
+		printf("%d: sending a signal to %d\n", getpid(), pid);
+		kill(pid, SIGUSR1);
 	}
 
 	munmap(addr, 1);
