9df3f141455b3c40cbdd9e324d82102a65d51225
Jan Pechanec <jp@devnull.cz>
2017-12-18 23:17:34 +0100

    Make it work on Linux as well.

--- pthreads/wrong-err-use.c
+++ pthreads/wrong-err-use.c
@@ -6,11 +6,23 @@
 #include <err.h>
 #include <pthread.h>
 
+void *
+thread(void *x)
+{
+	return (NULL);
+}
+
 int
 main(void)
 {
-	/* This should fail but it is not guaranteed. */
-	if (pthread_join((pthread_t)NULL, NULL) != 0)
+	pthread_t t;
+
+	/* We are lazy and assume this succeeds. */
+	(void) pthread_create(&t, NULL, thread, NULL);
+	/* We are lazy and assume this succeeds. */
+	(void) pthread_join(t, NULL);
+	/* This should fail as the thread has been already joined. */
+	if (pthread_join(t, NULL) != 0)
 		err(1, "pthread_join");
 	return (0);
 }
