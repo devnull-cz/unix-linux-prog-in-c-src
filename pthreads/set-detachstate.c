b720c2af6717a55dfc6b48b2fff5017abef2edc5
Jan Pechanec <jp@devnull.cz>
2017-12-19 00:08:23 +0100

    Better output and comments.

--- pthreads/set-detachstate.c
+++ pthreads/set-detachstate.c
@@ -1,5 +1,5 @@
 /*
- * Another way how to create detached threads. See also pthread-detach-join.c.
+ * Another way how to create detached threads.  See also pthread-detach-join.c.
  *
  * (c) jp@devnull.cz, vlada@devnull.cz
  */
@@ -19,8 +19,12 @@ thread(void *x)
 	if (*((int *)x) == 1)
 		poll(NULL, 0, 500);
 
+	/*
+	 * Note that we will only finish the first iteration as joining the
+	 * threads will fail.
+	 */
 	for (i = 0; i < 10; ++i) {
-		printf("thread %d (loop #%d)\n", *((int *) x), i);
+		printf("Thread %d (loop #%d).\n", *((int *) x), i);
 		sleep(1);
 	}
 
@@ -30,24 +34,23 @@ thread(void *x)
 int
 main(void)
 {
-	int e;
 	pthread_t t, t2;
 	pthread_attr_t a;
-	int n1 = 1, n2 = 2;
+	int e, n1 = 1, n2 = 2;
 
 	pthread_attr_init(&a);
 	pthread_attr_setdetachstate(&a, PTHREAD_CREATE_DETACHED);
 
 	pthread_create(&t, &a, thread, &n1);
 	pthread_create(&t2, &a, thread, &n2);
-	printf("threads created, will try to join them now\n");
+	printf("Threads created, will try to join them now.\n");
 
-	/* wait half a second for the threads to enter the loop */
-	(void) poll(NULL, 0, 800);
+	/* Wait a second for the threads to enter the loop. */
+	(void) poll(NULL, 0, 950);
 
+	/* These should fail. */
 	if ((e = pthread_join(t, NULL)) != 0)
 		warnx("pthread_join: thread 1: %s", strerror(e));
-
 	if ((e = pthread_join(t2, NULL)) != 0)
 		warnx("pthread_join: thread 2: %s", strerror(e));
 
