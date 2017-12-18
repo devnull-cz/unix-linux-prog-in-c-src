0126650e89b61c0cb71e89b78280f7f8e6557365
Jan Pechanec <jp@devnull.cz>
2017-12-18 23:10:57 +0100

    Another pthread example.

---
+++ pthreads/wrong-err-use.c
@@ -0,0 +1,16 @@
+/*
+ * Wrong use of the err(3) function.  As the Pthread API does not set errno,
+ * using err() is incorrect.  However, on different system, the message it
+ * prints may not be very helpful (try on Linux).
+ */
+#include <err.h>
+#include <pthread.h>
+
+int
+main(void)
+{
+	/* This should fail but it is not guaranteed. */
+	if (pthread_join(NULL, NULL) != 0)
+		err(1, "pthread_join");
+	return (0);
+}
