618ab4103faca7f9ee89532e37815870f802fb4d
Vladimir Kotal <vlada@devnull.cz>
2018-10-15 22:03:00 +0200

    add getopt_long() example from FreeBSD man page

---
+++ getopt/getopt_long.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <getopt.h>
+#include <fcntl.h>
+#include <err.h>
+
+static void
+usage()
+{
+	errx(1, "getopt_long: [-bf] [--buffy] [--fluoride] [--daggerset] "
+	    "[...]");
+}
+
+int
+main(int argc, char *argv[])
+{
+	int bflag,	ch, fd;
+	int daggerset;
+
+	/* options descriptor */
+	struct option longopts[] = {
+		{ "buffy",	no_argument,	  NULL,		'b' },
+		{ "fluoride",	required_argument, NULL,	'f' },
+		{ "daggerset",	no_argument,	  &daggerset,	1 },
+		{ NULL,		0,		  NULL,		0 }
+	};
+
+	bflag = 0;
+	while ((ch = getopt_long(argc, argv, "bf:", longopts, NULL)) != -1) {
+		switch (ch) {
+		case 'b':
+			bflag = 1;
+			break;
+		case 'f':
+			if ((fd = open(optarg, O_RDONLY, 0)) == -1)
+				err(1, "unable to open %s", optarg);
+			break;
+		case 0:
+			if (daggerset) {
+				fprintf(stderr, "Buffy will use her dagger to "
+				    "apply fluoride to dracula's teeth\n");
+			}
+			break;
+		default:
+			usage();
+		}
+	}
+
+	argc -= optind;
+	argv += optind;
+
+	return (0);
+}
