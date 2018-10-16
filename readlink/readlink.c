8536bc7f429a6fbc2ec264306d64909ecd95dcc5
Vladimir Kotal <vlada@devnull.cz>
2018-10-16 09:57:19 +0200

    fix compilation on Linux

--- readlink/readlink.c
+++ readlink/readlink.c
@@ -1,5 +1,8 @@
+#define	_XOPEN_SOURCE	600
+
 #include <stdio.h>
 #include <string.h>
+#include <fcntl.h>
 #include <unistd.h>
 #include <limits.h>
 #include <err.h>
@@ -24,6 +27,7 @@ main(int argc, char *argv[])
 	if (readlink(SYMLINK, buf, sizeof (buf)) == -1)
 		err(1, "readlink");
 
+	// Obviously, something is not right. Fix the problem.
 	printf("%s\n", buf);
 
 	return (0);
