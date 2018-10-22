fb116f6a1982a0d91ef46b81c48ab747663561d5
Jan Pechanec <jp@devnull.cz>
2018-10-22 23:50:58 +0200

    Make it work even if the symlink does not exist.

--- readlink/readlink.c
+++ readlink/readlink.c
@@ -1,24 +1,27 @@
 #define	_XOPEN_SOURCE	600
 
+#include <sys/stat.h>
+
+#include <err.h>
+#include <fcntl.h>
+#include <limits.h>
 #include <stdio.h>
 #include <string.h>
-#include <fcntl.h>
 #include <unistd.h>
-#include <limits.h>
-#include <err.h>
-
 
 #define	SYMLINK	"foobar"
 
 int
 main(int argc, char *argv[])
 {
+	struct stat stbuf;
 	char buf[PATH_MAX];
 
 	(void) memset(buf, 'A', sizeof (buf));
 	buf[sizeof (buf) - 1] = '\0';
 
-	if (unlink(SYMLINK) != 0)
+	/* Remove the symlink if it exists. */
+	if (stat(SYMLINK, &stbuf) != -1 && unlink(SYMLINK) != 0)
 		err(1, "unlink");
 
 	if (symlink("/etc/blah", SYMLINK) == -1)
@@ -27,7 +30,7 @@ main(int argc, char *argv[])
 	if (readlink(SYMLINK, buf, sizeof (buf)) == -1)
 		err(1, "readlink");
 
-	// Obviously, something is not right. Fix the problem.
+	/* Obviously, something is not right.  Fix the problem. */
 	printf("%s\n", buf);
 
 	return (0);
