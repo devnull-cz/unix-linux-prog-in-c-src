a5368365d3c96418c3acf0bd8044ea11a18f661a
Jan Pechanec <jp@devnull.cz>
2018-10-22 22:51:06 +0200

    General improvements.

--- read/redirect.c
+++ read/redirect.c
@@ -1,45 +1,46 @@
 /*
  * Implement the following command:
  *
- *	cat <in >out
+ *	cat <argv1 >argv2
  *
  * It means that after running "./a.out" the program will copy the existing file
- * "in" to a newly created (or an existing truncated) file "out".
+ * in argv[1] to a newly created (or an existing truncated) file argv[2].
  *
  * Run like this:
  *
  *	cc redirect.c
- *	cp /etc/passwd in
- * 	rm -f out
- *	./a.out
- *	diff /etc/passwd out	# should not print any difference
+ *	./a.out /etc/passwd out
+ *	diff /etc/passwd out		# should not print any difference
  */
 #include <sys/stat.h>
 #include <sys/types.h>
 
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h>
+#include <assert.h>
 #include <err.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 int
-main(void)
+main(int argc, char **argv)
 {
 	int fd;
 
-	if ((fd = open("in", O_RDONLY)) == -1)
+	assert(argc == 3);
+
+	if ((fd = open(argv[1], O_RDONLY)) == -1)
 		err(1, "open");
 
-	/* Do the "<in" part. */
+	/* Do the "<argv1" part. */
 	close(0);
 	dup(fd);
 	/* Always close what you no longer need. */
 	close(fd);
 
-	/* Do the ">out" part. */
+	/* Do the ">argv2" part. */
 	close(1);
-	if (creat("out", 0666) == -1)
-		err(1, "creat");
+	if (open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0666) == -1)
+		err(1, "open");
 
 	/*
 	 * No need to use "if" for error checking.  If we get past this call we
