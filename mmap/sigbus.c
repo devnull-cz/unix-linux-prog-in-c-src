dc6e52af2038b8b8f0b29328423cbf158f9242b5
Jan Pechanec <jp@devnull.cz>
2018-11-12 21:20:58 +0100

    Minor improvement for mmap sigbus example.

--- mmap/sigbus.c
+++ mmap/sigbus.c
@@ -12,35 +12,39 @@
  *     4k page.
  */
 
+#include <assert.h>
+#include <err.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
 #include <sys/mman.h>
+#include <unistd.h>
 
 int
 main(void)
 {
-	int fd;
+	int fd, i;
 	char c = (char) 'a';
 	char *addr = NULL;
-	int i;
 
 	printf("pagesize = %ld bytes\n", sysconf(_SC_PAGE_SIZE));
 
 	fd = open("test.dat", O_CREAT | O_RDWR | O_TRUNC, 0666);
+	assert(fd != -1);
 
 	lseek(fd, 99, SEEK_SET);
 	write(fd, &c, 1);
 
-	if ((addr = mmap(0, 100, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0))
-	    == NULL) {
-		fprintf(stderr, "mmap failed");
-		exit(1);
-	}
+	addr = mmap(0, 100, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+	if (addr == NULL)
+		err(1, "mmap");
 
-	for (i = 4095; i < 16384; i++) {
-		printf("%d\n", i); /* use \n so that output is flushed. */
+	for (i = 4090; i < 16384; i++) {
+		/*
+		 * Use \n so that output is flushed.  We may fail in reading or
+		 * later in writing.  It depends what the adjacent page is.
+		 */
+		printf("%d byte 0x%02x\n", i, addr[i]);
 		addr[i] = 0;
 	}
 
