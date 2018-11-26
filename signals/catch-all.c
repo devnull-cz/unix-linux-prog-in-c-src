9b443b36aa5688e4fade22a275cedd1d858c3aa6
Jan Pechanec <jp@devnull.cz>
2018-11-26 15:07:41 +0100

    Another signal example.

---
+++ signals/catch-all.c
@@ -0,0 +1,26 @@
+/*
+ * Catch all signals.  Try with ^C, ^\, also try resizing your terminal (you
+ * should get SIGWINCH), you can also try "pkill catch-all" from another
+ * terminal.  Note that ^Z (SIGTSTP) is also caught.
+ *
+ * You can only get rid of this by killing it by SIGKILL from another terminal
+ * (pkill -9 catch-all).
+ */
+#include <signal.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int
+main(void)
+{
+	int sig;
+	sigset_t set;
+
+	sigfillset(&set);
+	sigprocmask(SIG_SETMASK, &set, NULL);
+
+	while (1) {
+		sigwait(&set, &sig);
+		printf("%d: got '%d'\n", getpid(), sig);
+	}
+}
