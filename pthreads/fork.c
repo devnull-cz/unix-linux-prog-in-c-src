2ce58ab23ce97b057c507c3cb9cbbb4eb9f219af
Jan Pechanec <jp@devnull.cz>
2018-12-10 22:07:59 +0100

    Check return values of pthread_join().

--- pthreads/fork.c
+++ pthreads/fork.c
@@ -7,20 +7,20 @@
  * (c) jp@devnull.cz
  */
 
+#include <err.h>
+#include <poll.h>
 #include <pthread.h>
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
-#include <poll.h>
-#include <err.h>
 
 #define	NUM_LOOP	5
+#define	NSECS		4
 
 void *
 thread(void *x)
 {
-	int i;
-
-	for (i = 0; i < NUM_LOOP; ++i) {
+	for (int i = 0; i < NUM_LOOP; ++i) {
 		printf("%d: thread %d (loop #%d).\n", getpid(),
 		    *((int *)x), i);
 		sleep(1);
@@ -33,28 +33,29 @@ main(void)
 {
 	pid_t pid;
 	pthread_t t1, t2;
-	int n1 = 1, n2 = 2;
+	int e, n1 = 1, n2 = 2;
 
 	(void) pthread_create(&t1, NULL, thread, &n1);
 	(void) pthread_create(&t2, NULL, thread, &n2);
 
 	printf("%d: main program before fork()\n", getpid());
 	sleep(1);
-	pid = fork();
 
-	if (pid == -1)
+	if ((pid = fork()) == -1)
 		err(1, "fork");
 
 	if (pid == 0) {
-		printf("%d: child sleeping for a few seconds...\n", getpid());
-		sleep(4);
+		printf("%d: child sleeping for %d seconds...\n", getpid(),
+		    NSECS);
+		sleep(NSECS);
 		printf("%d: child exiting\n", getpid());
 		return (0);
 	}
 
-	/* You should really check the return values here... */
-	(void) pthread_join(t1, NULL);
-	(void) pthread_join(t2, NULL);
+	if ((e = pthread_join(t1, NULL)) != 0)
+		warnx("pthread_join: %s", strerror(e));
+	if ((e = pthread_join(t2, NULL)) != 0)
+		warnx("pthread_join: %s", strerror(e));
 
 	return (0);
 }
