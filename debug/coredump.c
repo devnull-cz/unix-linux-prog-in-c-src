5a9382b425875ec18ed4ef3bc7ec1d0b027d32b9
Jan Pechanec <jp@devnull.cz>
2019-10-08 22:54:44 +0200

    Simplified.

--- debug/coredump.c
+++ debug/coredump.c
@@ -1,14 +1,16 @@
 /*
- * Will crash and generate a core dump.
+ * Will crash and may generate a core dump.
  *
- * You might want to run 'ulimit -c size' with reasonable non-zero size to
- * actually get the core file.
+ * You might want to run "ulimit -c size" with reasonable non-zero size to
+ * actually get the core file.  The easiest might be "ulimit -c unlimited" but
+ * the size of 1000 (0.5MB) should be enough for this.
  */
 
 void
 bad_memory_access(void)
 {
-	char *x = 0x00;
+	char *x = NULL;
+
 	x[0] = 'A';
 }
 
@@ -16,7 +18,5 @@ int
 main(void)
 {
 	bad_memory_access();
-
 	/* NOTREACHED */
-	return (0);
 }
