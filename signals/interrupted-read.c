c968b9283c270218ca4a24e5ec6c635e335a1b27
Jan Pechanec <jp@devnull.cz>
2019-11-19 23:12:28 +0100

    Option for SA_RESTART.

--- signals/interrupted-read.c
+++ signals/interrupted-read.c
@@ -1,6 +1,6 @@
 /*
- * An example on an interrupted read(2).  Run the program, then send it SIGTERM
- * from another terminal.  read(2) should return an error, with EINTR errno:
+ * An example on an interrupted read(2).  Run the program, then kill it from
+ * another terminal.  read(2) should return an error, with EINTR errno.
  *
  *  $ ./a.out
  *  My PID is 30794.
@@ -8,16 +8,17 @@
  *  TERM signal caught !
  *  a.out: read: Interrupted system call
  *
+ * Use -r option for SA_RESTART.
+ *
  * (c) jp@devnull.cz
  */
-
 #define	_XOPEN_SOURCE	700
 
+#include <err.h>
 #include <signal.h>
-#include <unistd.h>
 #include <stdio.h>
 #include <string.h>
-#include <err.h>
+#include <unistd.h>
 
 #define	MESSAGE "TERM signal caught !\n"
 
@@ -25,25 +26,28 @@ void
 term_handler(int sig)
 {
 	/* write(2) is safe to use from signal handlers. */
-	write(1, MESSAGE, strlen(MESSAGE));
+	write(1, MESSAGE, sizeof (MESSAGE) - 1);
 }
 
 int
-main(void)
+main(int argc, char **argv)
 {
 	char c;
-	int ret, fd[2];
+	int fd[2];
 	struct sigaction act = { 0 };
 
-	printf("My PID is %d.\n", getpid());
+	(void)argc;
+
+	printf("My PID is %d (use -r for SA_RESTART).\n", getpid());
+
+	if (argv[1] != NULL && strcmp(argv[1], "-r") == 0)
+		act.sa_flags = SA_RESTART;
 
 	act.sa_handler = term_handler;
 	sigaction(SIGTERM, &act, NULL);
 
 	if (pipe(fd) == -1)
 		err(1, "pipe");
-	ret = read(fd[0], &c, 1);
-	if (ret == -1)
+	if (read(fd[0], &c, 1) == -1)
 		err(1, "read");
-	return (0);
 }
