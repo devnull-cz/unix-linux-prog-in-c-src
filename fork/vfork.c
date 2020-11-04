4a2d69f48efa63281725b5d345f13983e3796192
Jan Pechanec <jp@devnull.cz>
2020-11-04 22:19:44 +0100

    Simplified.

--- fork/vfork.c
+++ fork/vfork.c
@@ -1,13 +1,13 @@
 /*
- * vfork() is obsolete but let's see how it works. This is the same program as
+ * vfork() is obsolete but let's see how it works.  This is the same program as
  * fork/fork.c aside from changing fork() into vfork() and letting both
- * processes execute the same amount of iterations. Now, the parent is
- * stopped until the child finishes which is easily seen from the output.
+ * processes execute the same amount of iterations.  Now, the parent is stopped
+ * until the child finishes which is easily seen from the output.
  *
- * See vfork() man page for details. This program could core dump if return
+ * See vfork() man page for details.  This program could core dump if return
  * was used since a child is not supposed to use the parent's process address
- * space any other way than to exec() or _exit(). By using return, the
- * parent's stack would be changed. _exit() is used in favour of exit() to
+ * space any other way than to exec() or _exit().  By using the return operator,
+ * the parent's stack would be changed.  _exit() is used in favour of exit() to
  * avoid any atexit() routines to be called on behalf of the child.
  *
  * (c) jp@devnull.cz
@@ -15,9 +15,10 @@
 
 #define	_XOPEN_SOURCE 600	// needed for vfork()
 
+#include <err.h>
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int
 main(void)
@@ -27,8 +28,7 @@ main(void)
 
 	switch (pid = vfork()) {
 	case -1:
-		fprintf(stderr, "error\n");
-		exit(1);
+		err(1, "fork");
 	case 0:
 		printf("I'm child, PID is %d\n", getpid());
 		name = "child";
@@ -46,7 +46,7 @@ main(void)
 
 	/*
 	 * If you return from here instead of exit()ing it may core dump based
-	 * on the system used. It does so on FreeBSD 6.1, it doesn't on
+	 * on the system used.  It does so on FreeBSD 6.1, it doesn't on
 	 * Solaris 10.
 	 */
 	_exit(0);
