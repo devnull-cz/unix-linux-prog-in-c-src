754ecaed097a2b15bdefb057c32fbd9318ad5907
Vladimir Kotal <vlada@devnull.cz>
2020-11-12 00:17:07 +0100

    make the demonstration more usable

--- signals/sigchld.c
+++ signals/sigchld.c
@@ -1,42 +1,58 @@
 /*
- * The SIGCHLD signal is always delivered to the parent process except
- * in case it is ignored.
+ * Run the program with and without argument and compare the process trees.
  */
 
-#include <stdio.h>
-#include <signal.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
+#define	_XOPEN_SOURCE	700
 
-void
-handler(int s)
-{
-	write(1, "got it\n", 7);
-}
+#include <err.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 int
 main(int argc, char *argv[])
 {
-	pid_t pid;
+	char c;
+	sigset_t set;
+	int pid, sig;
+	int ignore = 0;
 
-#ifdef HANDLE_SIGCHLD
-	struct sigaction act;
-	// act.sa_handler = handler;
-	act.sa_handler = SIG_IGN;
-	sigemptyset(&act.sa_mask);
-	act.sa_flags = 0;
-	sigaction(SIGCHLD, &act, NULL);
-#endif
-
-	if ((pid = fork()) == 0) {
-		sleep(1);
-	} else {
-		int status;
-		printf("forked %d\n", pid);
-		// wait(&status);
-		waitpid(pid, &status, 0);
+	if (argc > 1) {
+		printf("will ignore SIGCHLD\n");
+		ignore = 1;
 	}
 
-	return (0);
+	switch (pid = fork()) {
+	case -1:
+		err(1, "fork");
+	case 0:
+		printf("%d: this is a child\n", getpid());
+		pause();
+		break;
+	default:
+		printf("%d: this is a parent\n", getpid());
+		printf("%d: type anything (only the 1st char matters)...\n",
+		    getpid());
+		if (read(0, &c, 1) != 1)
+			err(1, "read");
+
+		if (ignore) {
+			struct sigaction s_action;
+			(void) sigemptyset(&s_action.sa_mask);
+			s_action.sa_handler = SIG_IGN;
+			s_action.sa_flags = 0;
+			(void) sigaction(SIGCHLD, &s_action, NULL);
+		}
+
+		printf("%d: sending a signal to %d\n", getpid(), pid);
+		kill(pid, SIGTERM);
+		// wait(NULL);
+		printf("%d: waiting for two signals\n", getpid());
+		pause();
+		printf("got one\n");
+		pause();
+	}
 }
