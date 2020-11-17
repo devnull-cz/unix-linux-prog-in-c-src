bc686106f15e7b7a05b15dbf1a2eb360f24e9774
Jan Pechanec <jp@devnull.cz>
2020-11-17 20:39:33 +0100

    Improved comment.

--- mmap/map-nocore.c
+++ mmap/map-nocore.c
@@ -1,23 +1,25 @@
 /*
- * Example on MAP_NOCORE use. Build it, run it on the background, dump it (eg.,
- * "kill -3 <PID"), and use strings(1) with "-a" on it to grep "my-PIN". It
- * should be there for the 1st case and it should be clean for the 2nd case.
+ * Example on MAP_NOCORE use. On FreeBSD, build it, run it and wait for the core
+ * dump.  Then use strings(1) with "-a" on the core to grep "my-PIN".  It should
+ * be there for argv1 == 0 and it should be clean for argv1 == 1.
  *
- * FreeBSD supports the flag, Solaris does not. Note that gcore(1) would write
- * the whole process to a file, including the NOCORE segment. That's quite
- * logical since this operation can be done by somebody who can examine the
- * process memory anyway. The objective is that the mmaped page is not in the
- * core dump caused by the kernel (on SIGSEGV etc.).
+ * FreeBSD supports the flag but neither Linux, Solaris nor macOS do.  Note that
+ * gcore(1) on Solaris would write the whole process to a file, including the
+ * NOCORE segment.  That's quite logical since this operation can be done by
+ * somebody who can examine the process memory anyway.  The objective is that
+ * the mmap'ed page is not in the core dump caused by the kernel (on SIGSEGV
+ * etc.).
  */
 
 #define	_DEFAULT_SOURCE	// to get MAP_ANONYMOUS on Linux
 
+#include <err.h>
+#include <fcntl.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <err.h>
 #include <sys/mman.h>
+#include <unistd.h>
 
 int
 main(int argc, char *argv[])
@@ -41,10 +43,10 @@ main(int argc, char *argv[])
 		err(1, "mmap");
 
 	/*
-	 * The password is "my-PIN". We put the PIN there the way we use because
-	 * if we used a static string it would end up in a .data segment.
-	 * Like this it ends up in .text segment anyway, for any real world use
-	 * it would come from external source (pipe, stdin, ...).
+	 * The password is "my-PIN".  We put the PIN there like that because if
+	 * we used a static string it would end up in the .data segment.  Now it
+	 * ends up in .text segment.  Anyway, for any real world use it would
+	 * come from external source (pipe, stdin, ...).
 	 */
 	addr[0] = 'm';
 	addr[1] = 'y';
@@ -54,7 +56,5 @@ main(int argc, char *argv[])
 	addr[5] = 'N';
 	addr[6] = '\0';
 
-	sleep(99);
-
-	return (0);
+	kill(getpid(), SIGQUIT);
 }
