8af9a8c840d4176ce4d60b0f739dec6f09d899d3
Vladimir Kotal <vlada@devnull.cz>
2020-12-09 17:25:15 +0100

    add cleanup example

---
+++ pthreads/pthread-cleanup.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+
+static void
+cleanup(void *p)
+{
+	printf("cleanup ! cleanup ! everybody let's cleanup !\n");
+}
+
+void *
+thread(void *x)
+{
+	int i;
+
+	for (i = 0; i < 5; ++i) {
+		int doit = 0;
+		pthread_cleanup_push(cleanup, NULL);
+		(void) printf("thread loop #%d\n", i);
+		if (i > 2)
+			doit = 1;
+		(void) sleep(1);
+		pthread_cleanup_pop(doit);
+	}
+	return (NULL);
+}
+
+int
+main(int argc, char *argv[])
+{
+	pthread_t t;
+
+	(void) pthread_create(&t, NULL, thread, NULL);
+	pthread_join(t, NULL);
+
+	return (0);
+}
