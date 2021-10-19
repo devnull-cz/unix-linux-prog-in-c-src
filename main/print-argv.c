7733173841e0fc923847ebd5570566ab4fbdacdf
Jan <jp@devnull.cz>
2021-10-19 18:58:12 +0200

    Another way to print argv.

--- main/print-argv.c
+++ main/print-argv.c
@@ -1,15 +1,14 @@
 /*
  * Print all strings from argv[].
+ *
+ * You can do it multiple ways.
  */
-
 #include <stdio.h>
 
 int
 main(int argc, char **argv)
 {
 	int i = 0;
-
-	/* You can do it both ways. */
 
 	printf("argv guided:\n");
 	while (argv[i] != NULL) {
@@ -21,5 +20,11 @@ main(int argc, char **argv)
 	for (i = 0; i < argc; ++i)
 		printf("[%d] %s\n", i, argv[i]);
 
-	return (0);
+	/*
+	 * And, as the last one which keeps the state in argv, we can even do
+	 * the following.
+	 */
+	printf("argv guided simplified:\n");
+	while (*argv)
+		printf("%s\n", *argv++);
 }
