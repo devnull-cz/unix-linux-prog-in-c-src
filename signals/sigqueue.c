56ba02694536fdf90935375b95f546c7b43690ea
Jan <jp@devnull.cz>
2021-12-01 22:29:36 +0100

    Improved.

--- signals/sigqueue.c
+++ signals/sigqueue.c
@@ -4,22 +4,26 @@
  *
  * Compile and run:
  *
- *     $ cc sigqueue.c
- *     $ ./a.out
- *     $ echo $?
- *
- * (c) vlada@devnull.cz
+ *	$ cc sigqueue.c
+ *	$ ./a.out 98
+ *	367968 sleeping
+ *	367969 waiting
+ *	367968 sending SIGINT signal to 367969
+ *	367969 exiting with received value of 98
+ *	$ echo $?
+ *	99
  */
 
 #define	_XOPEN_SOURCE	700
 
-#include <stdio.h>
-#include <string.h>
+#include <err.h>
 #include <signal.h>
-#include <unistd.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <unistd.h>
 
 #ifndef _POSIX_REALTIME_SIGNALS
 #error	"Sorry, POSIX.4 real time signals extensions missing."
@@ -35,18 +39,20 @@ handler(int sig, siginfo_t *info, void *foo)
 }
 
 int
-main(void)
+main(int argc, char **argv)
 {
 	struct sigaction act = { 0 };
 	union sigval val;
 	pid_t pid;
 	int status;
 
-	switch (pid = fork()) {
-	case -1:
-		perror("fork");
-		break;
-	case 0:
+	if (argc != 2)
+		errx(1, "One small numerical argument required");
+
+	if ((pid = fork()) == -1)
+		err(1, "fork");
+
+	if (pid == 0) {
 		/*
 		 * We have to use the SIGINFO extension to be able to pass a
 		 * value to a signal handler.
@@ -57,24 +63,22 @@ main(void)
 
 		printf("%d waiting\n", getpid());
 		pause();
-		printf("%d exiting with received %d\n", getpid(), num);
-		exit(num);
-		break;
-	default:
-		/*
-		 * We should block SIGINT in the parent and unblock it in the
-		 * child only after the handler is set otherwise the signal will
-		 * terminate the child before it can setup the handler.  Use
-		 * sleep as a crude way to avoid this.
-		 */
-		printf("%d sleeping\n", getpid());
-		sleep(1);
-
-		val.sival_int = 42;
-		printf("%d sending SIGINT signal to %d\n", getpid(), pid);
-		sigqueue(pid, SIGINT, val);
-		waitpid(pid, &status, 0);
-		exit(WEXITSTATUS(status));
-		break;
+		printf("%d exiting with received value of %d\n", getpid(), num);
+		exit(num + 1);
 	}
+
+	/*
+	 * We should block SIGINT in the parent and unblock it in the
+	 * child only after the handler is set otherwise the signal will
+	 * terminate the child before it can setup the handler.  Use
+	 * sleep as a crude way to avoid this.
+	 */
+	printf("%d sleeping 1 second\n", getpid());
+	sleep(1);
+
+	val.sival_int = atoi(argv[1]);
+	printf("%d sending SIGINT signal to %d\n", getpid(), pid);
+	sigqueue(pid, SIGINT, val);
+	waitpid(pid, &status, 0);
+	exit(WEXITSTATUS(status));
 }
